22e0ae76e113baec3d4a0498f5f07de11358617d
Matheus Braga Dias <matheusbragaprofissional@gmail.com>
2023-09-09 10:54:24 -0300

    Ip version

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { HttpClient } from '@angular/common/http'
 
 @Component({
   standalone: true,
@@ -8,6 +9,7 @@ import { NxWelcomeComponent } from './nx-welcome.component';
   selector: 'plano-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  providers: [HttpClient]
 })
 export class AppComponent {
   title = 'frontend';
